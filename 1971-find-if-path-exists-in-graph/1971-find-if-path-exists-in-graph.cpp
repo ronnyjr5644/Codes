b8ca28301d89f2492c9b702a0b812c7670b92a5e
Raunak Raj <59229330+ronnyjr5644@users.noreply.github.com>
2022-12-19 22:13:17 +0530

    Time: 684 ms (88.28%), Space: 214 MB (23.61%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,39 +1,62 @@
 class Solution {
 public:
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        //im sir graphs alot
+//         //im sir graphs alot
+//         vector<vector<int>>adj(n);
+//         for(int i=0;i<edges.size();i++){
+//             adj[edges[i][0]].push_back(edges[i][1]);
+//             adj[edges[i][1]].push_back(edges[i][0]);
+//         }
+        
+//         //chaliye bfs lagate hai
+//         queue<int>q;
+//         vector<int>vis(n,0);
+//         //source dikh rha hai
+//         q.push(source);
+//         //dal dia queue me\
+//         //nhi dalna tha
+        
+//         vis[source]=1;
+//         //koi na ab visited ko 1 karna as we have traversed it
+//         //chaliye traverse karte hai
+//         while(!q.empty()){
+//             auto top=q.front();
+//             q.pop();
+//             if(top==destination){
+//                 return true;
+//             }
+//             for(auto it:adj[top]){
+//                 if(vis[it]==0){
+//                     q.push(it);
+//                     vis[it]=1;
+//                 }
+//             }
+            
+//         }
+//         return false;
+        //chaliye shuru karte hai depth first search ka khel
         vector<vector<int>>adj(n);
         for(int i=0;i<edges.size();i++){
             adj[edges[i][0]].push_back(edges[i][1]);
             adj[edges[i][1]].push_back(edges[i][0]);
         }
-        
-        //chaliye bfs lagate hai
-        queue<int>q;
         vector<int>vis(n,0);
-        //source dikh rha hai
-        q.push(source);
-        //dal dia queue me\
-        //nhi dalna tha
-        
-        vis[source]=1;
-        //koi na ab visited ko 1 karna as we have traversed it
-        //chaliye traverse karte hai
-        while(!q.empty()){
-            auto top=q.front();
-            q.pop();
-            if(top==destination){
+        return solve(adj,vis,source,destination);
+    }
+    bool solve(vector<vector<int>>&adj,vector<int>&vis,int curr,int end){
+        if(curr==end){
+            return true;
+        }
+        if(vis[curr]==1){
+            return false;
+        }
+        vis[curr]=1;
+        for(auto it:adj[curr]){
+            if(solve(adj,vis,it,end)==1){
                 return true;
             }
-            for(auto it:adj[top]){
-                if(vis[it]==0){
-                    q.push(it);
-                    vis[it]=1;
-                }
-            }
-            
+        
         }
         return false;
-        
     }
 };
