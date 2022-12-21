08b166fcdcfac9cd2837e7b9a2773c5aea5859bd
Raunak Raj <59229330+ronnyjr5644@users.noreply.github.com>
2022-12-22 02:10:19 +0530

    Time: 228 ms (87.21%), Space: 64.6 MB (82.33%) - LeetHub

---
+++ 0886-possible-bipartition/0886-possible-bipartition.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
+        int i, j, k, u, v, s;
+        vector<vector<int>> graph(n);
+        queue<int> q;
+        vector<int> visited(n, 0);
+        for(i = 0; i < dislikes.size(); i++){
+            u = dislikes[i][0] - 1;
+            v = dislikes[i][1] - 1;
+            graph[u].push_back(v);
+            graph[v].push_back(u);
+        }
+        
+        for(i = 0; i < n; i++){
+            if(visited[i] != 0){
+                continue;
+            }
+            q.push(i);
+            visited[i] = 1;
+            while(!q.empty()){
+                s = q.size();
+                for(j = 0; j < s; j++){
+                    u = q.front();
+                    q.pop();
+                    for(k = 0; k < graph[u].size(); k++){
+                        v = graph[u][k];
+                        if(visited[v] == 0){
+                            q.push(v);
+                            visited[v] = visited[u] == 1 ? 2 : 1;
+                        }
+                        
+                        if(visited[v] == visited[u]){
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true; 
+    }
+};
