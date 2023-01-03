c72d55c3501b23322ad2e0ed91fe2b5cc07129ae
Raunak Raj <59229330+ronnyjr5644@users.noreply.github.com>
2023-01-03 15:01:54 +0530

    Added solution - LeetHub

--- Minimize number of Students to be removed - GFG/minimize-number-of-students-to-be-removed.cpp
+++ Minimize number of Students to be removed - GFG/minimize-number-of-students-to-be-removed.cpp
@@ -9,20 +9,47 @@ using namespace std;
 class Solution {
   public:
     int removeStudents(int H[], int N) {
-         vector<int> list;
-    list.push_back(H[0]);
-    for (int i = 1; i < N; i++) {
-        int num = H[i];
-        if (num < list[0]) {
-            list[0] = num;
-        } else if (num > list[list.size() - 1]) {
-            list.push_back(num);
-        } else {
-            auto it = lower_bound(list.begin(), list.end(), num);
-            *it = num;
+   
+    int dp[N + 1];
+    int ans = 0;
+    fill(dp, dp + N + 1, INT_MAX);
+    dp[0] = INT_MIN;
+    for (int i = 0; i < N; i++) {
+        int key = H[i];
+        int low = 0, high = N + 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (dp[mid] >= key && dp[mid - 1] < key) {
+                dp[mid] = key;
+                break;
+            } else if (dp[mid] < key) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
         }
     }
-    return N - list.size();
+    for (int i = N; i >= 0; i--) {
+        if (dp[i] != INT_MAX) {
+            ans = i;
+            break;
+        }
+    }
+    return N - ans;
+     //      vector<int> list;
+    // list.push_back(H[0]);
+    // for (int i = 1; i < N; i++) {
+    //     int num = H[i];
+    //     if (num < list[0]) {
+    //         list[0] = num;
+    //     } else if (num > list[list.size() - 1]) {
+    //         list.push_back(num);
+    //     } else {
+    //         auto it = lower_bound(list.begin(), list.end(), num);
+    //         *it = num;
+    //     }
+    // }
+    // return N - list.size();
     }
 };
 
