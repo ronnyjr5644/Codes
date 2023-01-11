058ad22660d0ad9e7f58c4843a7f95a829599a95
Raunak Raj <59229330+ronnyjr5644@users.noreply.github.com>
2023-01-11 19:30:54 +0530

    Time: 195 ms (81.88%), Space: 65 MB (37.58%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    int solve(int node,const vector<vector<int>>&tree,const vector<bool>& hasApple, vector<bool>&vis){
+    int solve(int node,vector<vector<int>>&adj,vector<bool>& hasApple, vector<bool>&vis){
         vis[node]=true;
         int depth=0;
-        for(const auto & ch:tree[node]){
+        for(auto ch:adj[node]){
             if(vis[ch]==false){
-                depth=depth+solve(ch,tree,hasApple,vis);
+                depth=depth+solve(ch,adj,hasApple,vis);
             }
         }
         return (depth || hasApple[node])?depth+1:0;
     }
     int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
-        vector<vector<int>>tree(n);
+        vector<vector<int>>adj(n);
         vector<bool>vis(n);
-        for(const auto &it:edges){
-            tree[it[0]].push_back(it[1]);
-            tree[it[1]].push_back(it[0]);
+        for(auto it:edges){
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
         }
-        return max(0,2*(solve(0,tree,hasApple,vis)-1));
+        return max(0,2*(solve(0,adj,hasApple,vis)-1));
     }
 };
