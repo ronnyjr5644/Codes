48654417127f559b9fc036b022dd96b8e1643cac
Raunak Raj <59229330+ronnyjr5644@users.noreply.github.com>
2023-01-31 13:49:56 +0530

    Time: 293 ms (50.65%), Space: 27.4 MB (24.67%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
      int dp[1001][1001];
-     int recur(vector<vector<int>>&grp , int i , int n , int maxiAge){
-        if(i==n)
+     int solve(vector<vector<int>>&arr,int i,int n,int maxiAge){
+        if(i==n){
             return 0;
+        }
         if(dp[i][maxiAge]!=-1){
             return dp[i][maxiAge];
         }
-        if(grp[i][1]>=maxiAge){
-            return dp[i][maxiAge]=max(grp[i][0]+recur(grp,i+1,n,grp[i][1]),recur(grp,i+1,n,maxiAge));
+        if(arr[i][1]>=maxiAge){
+            return dp[i][maxiAge]=max(arr[i][0]+solve(arr,i+1,n,arr[i][1]),solve(arr,i+1,n,maxiAge));
         }
-        return dp[i][maxiAge] = recur(grp,i+1,n,maxiAge);
+        return dp[i][maxiAge]=solve(arr,i+1,n,maxiAge);
     }
     
     int bestTeamScore(vector<int>& scores, vector<int>& ages){
         memset(dp,-1,sizeof(dp));
-        vector<vector<int>>grp;
+        vector<vector<int>>arr;
         for(int i=0;i<scores.size();i++){
-            grp.push_back({scores[i],ages[i]});
+            arr.push_back({scores[i],ages[i]});
         }
-        sort(grp.begin(),grp.end());     
-        return recur(grp,0,ages.size(),0);
+        sort(arr.begin(),arr.end());     
+        return solve(arr,0,ages.size(),0);
     }
 };
