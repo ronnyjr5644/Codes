4301185208f98833a60cce6f37ab23d6ce583130
Raunak Raj <59229330+ronnyjr5644@users.noreply.github.com>
2023-02-02 19:38:05 +0530

    Added solution - LeetHub

---
+++ Maximum XOR of two numbers in an array - GFG/maximum-xor-of-two-numbers-in-an-array.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            String s = br.readLine();
+            String[] S = s.split(" ");
+            int[] v = new int[n];
+            for(int i = 0; i < n; i++)
+            {
+                v[i] = Integer.parseInt(S[i]);
+            }
+            Solution ob = new Solution();
+           System.out.println(ob.max_xor(v, n));
+            
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public static int max_xor(int arr[], int n)
+    {
+          int max=0;
+        for (int i=0 ;i<arr.length;i++){
+            for (int j=0;j<arr.length&&i!=j;j++){
+                max=Math.max(arr[i]^arr[j],max);
+            }
+        }
+
+        return max;
+    }
+}
