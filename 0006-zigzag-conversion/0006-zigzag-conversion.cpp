1498726a6cc8079c630a52b233c07e21fa81c7a9
Raunak Raj <59229330+ronnyjr5644@users.noreply.github.com>
2023-02-03 19:03:11 +0530

    Time: 69 ms (17.88%), Space: 95.1 MB (5.08%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string convert(string s, int nRows) {
+         if(nRows <= 1) return s;
+        string result = ""; 
+        int cycle = 2 * nRows - 2;
+        for(int i = 0; i < nRows; ++i)
+        {
+            for(int j = i; j < s.length(); j = j + cycle){
+               result = result + s[j];
+               int secondJ = (j - i) + cycle - i;
+               if(i != 0 && i != nRows-1 && secondJ < s.length())
+                   result = result + s[secondJ];
+            }
+        }
+        return result;
+    }
+};
