5327e54ea12d95ad200798218cd4856b72bab191
Raunak Raj <59229330+ronnyjr5644@users.noreply.github.com>
2023-04-08 19:45:43 +0530

    Time: 8 ms (32.28%), Space: 8.8 MB (65.38%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -21,32 +21,15 @@ public:
 
 class Solution {
 public:
-     Node* dfs(Node* cur,unordered_map<Node*,Node*>& mp)
-    {
-        vector<Node*> neighbour;
-        Node* clone=new Node(cur->val);
-        mp[cur]=clone;
-            for(auto it:cur->neighbors)
-            {
-                if(mp.find(it)!=mp.end())   
-                {
-                    neighbour.push_back(mp[it]);    
-                }
-                else
-                    neighbour.push_back(dfs(it,mp));
-            }
-            clone->neighbors=neighbour;
-            return clone;
-    }
-    Node* cloneGraph(Node* node) {
-        unordered_map<Node*,Node*> mp;
-        if(node==NULL)
-            return NULL;
-        if(node->neighbors.size()==0)  
-        {
-            Node* clone= new Node(node->val);
-            return clone; 
-        }
-        return dfs(node,mp);
+    unordered_map<Node*,Node*> hash;
+  Node *cloneGraph(Node *node) {
+       if (!node) return node;
+       if(hash.find(node) == hash.end()) {
+           hash[node] = new Node(node -> val);
+           for (auto x : node -> neighbors) {
+                (hash[node] -> neighbors).push_back( cloneGraph(x) );
+           }
+       }
+       return hash[node];
     }
 };
