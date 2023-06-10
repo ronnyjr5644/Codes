87e6f31436c2e630a9998c56064aeacdf03510f4
Raunak Raj <59229330+ronnyjr5644@users.noreply.github.com>
2023-06-10 13:39:29 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (46.01%) - LeetHub

--- 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.cpp
+++ 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-   long sum(long n){ //sum of n numbers
+   long sum(long n){ 
         return (n*(n+1))/2;
     }
     
@@ -8,11 +8,11 @@ public:
         long l = 1, r=ms, res=0, i=idx, j=n-idx-1;
         
         while(l<=r){
-            long mid = l+(r-l)/2, m=mid-1; //mid is the max number we assumed at the index
-            long ls = i>=m?(sum(m) + i-m):(sum(m) - sum(m-i)); //finding left sum i.e., mid-1 + mid -2 +...
+            long mid = l+(r-l)/2, m=mid-1; 
+            long ls = i>=m?(sum(m) + i-m):(sum(m) - sum(m-i)); 
             long rs = j>=m?(sum(m) + j-m):(sum(m) - sum(m-j));
-            // cout<<ls<<" "<<rs<<" "<<mid<<endl;
-            if(ls+rs+mid<=ms){ //checking for validity
+        
+            if(ls+rs+mid<=ms){ 
                 res = mid;
                 l=mid+1;
             }
