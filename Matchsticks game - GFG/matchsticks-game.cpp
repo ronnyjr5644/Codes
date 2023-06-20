8b9f9ec7e9ceccd3df2e384abb6776eab92a9718
Raunak Raj <59229330+ronnyjr5644@users.noreply.github.com>
2023-06-20 22:18:33 +0530

    Added solution - LeetHub

---
+++ Matchsticks game - GFG/matchsticks-game.cpp
@@ -0,0 +1,29 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int matchGame(long long N) {
+       int res = N % 5;
+        
+        return res == 0 ? -1: res;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.matchGame(N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
