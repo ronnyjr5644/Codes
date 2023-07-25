5020154a8e6567af0a889b1053a41cef3b968305
Raunak Raj <59229330+ronnyjr5644@users.noreply.github.com>
2023-07-25 10:08:01 +0530

    Time: 211 ms (69.94%), Space: 52.2 MB (28.83%) - LeetHub

---
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int PeakIndexInMountainArray(int[] arr) {
+        
+       int left = 0;
+        int right = arr.Length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] < arr[mid + 1]) {
+             
+                left = mid + 1;
+            } else {
+              
+                right = mid;
+            }
+        }
+
+   
+        return left;
+    }
+}
