9a9d55f66623a72c524eabdf9c5744c58b357e1a
Raunak Raj <59229330+ronnyjr5644@users.noreply.github.com>
2023-08-20 14:59:26 +0530

    Added solution - LeetHub

--- Number of occurrence - GFG/number-of-occurrence.cpp
+++ Number of occurrence - GFG/number-of-occurrence.cpp
@@ -11,20 +11,59 @@ public:
 	/* if x is present in arr[] then returns the count
 		of occurrences of x, otherwise returns 0. */
 	int count(int arr[], int n, int x) {
-	    int res=0;
-	    int i=0;
-	    int flag=false;
-	    while(i<n){
-	        if(arr[i]==x){
-	            flag=true;
-	            res++;
-	        }
-	        if(arr[i]!=x && flag==true){
-	            break;
-	        }
-	        i++;
-	    }
-	    return res;
+	   // int res=0;
+	   // int i=0;
+	   // int flag=false;
+	   // while(i<n){
+	   //     if(arr[i]==x){
+	   //         flag=true;
+	   //         res++;
+	   //     }
+	   //     if(arr[i]!=x && flag==true){
+	   //         break;
+	   //     }
+	   //     i++;
+	   // }
+	   // return res;
+	   
+	   int left, right,mid,first,last;
+	   left=0;
+	   right=n-1;
+	   first=last=-1;
+	   while(left<=right){
+	       mid=left+(right-left)/2;
+	       if(arr[mid]==x){
+	           first=mid;
+	           right=mid-1;
+	       }
+	       else if(arr[mid]>x){
+	           right=mid-1;
+	       }
+	       else{
+	           left=mid+1;
+	       }
+	   }
+	   if(first== -1){
+	       return 0;
+	   }
+	   left=0;
+	   right=n-1;
+	   
+	   while(left<=right){
+	       mid=left+(right-left)/2;
+	       if(arr[mid]==x){
+	           last=mid;
+	           left=mid+1;
+	       }
+	       else if(arr[mid]>x){
+	           right=mid-1;
+	       }
+	       else{
+	           left=mid+1;
+	       }
+	   }
+	   return last-first+1;
+	   
 	    
 	}
 };
