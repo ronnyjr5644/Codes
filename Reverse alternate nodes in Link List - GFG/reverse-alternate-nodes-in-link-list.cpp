4b59462e97931ec33e58bc8b7fc63ad3b7a917b9
Raunak Raj <59229330+ronnyjr5644@users.noreply.github.com>
2023-10-06 19:54:59 +0530

    Added solution - LeetHub

---
+++ Reverse alternate nodes in Link List - GFG/reverse-alternate-nodes-in-link-list.cpp
@@ -0,0 +1,141 @@
+//{ Driver Code Starts
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+using namespace std;
+/* A linked list node */
+
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+struct Node *start = NULL;
+
+/* Function to print nodes in a given linked list */
+void printList(struct Node *node)
+{
+    while (node != NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+    
+}
+
+void insert()
+{
+    int n,value;
+    cin>>n;
+    struct Node *temp;
+    for(int i=0;i<n;i++)
+    {
+        cin>>value;
+        if(i==0)
+        {
+            start = new Node(value);
+            temp = start;
+            continue;
+        }
+        else
+        {
+            temp->next = new Node(value);
+            temp = temp->next;
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+/*
+  reverse alternate nodes and append at the end
+  The input list will have at least one element  
+  Node is defined as 
+  struct Node
+  {
+      int data;
+      struct Node *next;
+    
+      Node(int x){
+        data = x;
+        next = NULL;
+      }
+    
+   };
+
+*/
+class Solution
+{
+    public:
+    void rearrange(struct Node *odd)
+    {
+        
+         struct Node *x=odd;
+         int n=0;
+          while(x!=nullptr){
+           n++;
+            x=x->next;
+           
+        }
+        x=odd;
+int* temp = new int[n];
+    int* y = new int[n];
+ int m=0;
+        y[m]=x->data;
+        m++;
+        x=x->next;
+        int l=0;
+       
+        while(x!=nullptr){
+            temp[l]=x->data;
+            l++;
+            x=x->next;
+            if(x!=nullptr){
+            y[m]=x->data;
+            m++;
+           
+                x=x->next;
+            }
+            
+        }
+        x=odd;
+       for(int i=0;i<m;i++){
+           x->data=y[i];
+           x=x->next;
+       }
+        
+       for(int i=l-1;i>=0;i--){
+           x->data=temp[i];
+           x=x->next;
+       }
+        
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--) {
+        insert();
+        Solution ob;
+        ob.rearrange(start);
+        printList(start);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
