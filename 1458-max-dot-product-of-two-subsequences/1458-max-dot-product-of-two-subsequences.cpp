6fe88194979d8dbcd52167abe52a2f337d03e2d3
Raunak Raj <59229330+ronnyjr5644@users.noreply.github.com>
2023-10-08 12:32:29 +0530

    Time: 14 ms (93.06%), Space: 11 MB (74.29%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
@@ -1,5 +1,14 @@
 class Solution {
 public:
+     int solve(int i, int j) {
+        if (i == nums1.size() || j == nums2.size()) return INT_MIN;
+        if (dp[i][j] != -1) return dp[i][j];
+        int res = INT_MIN;
+        res = max(res, max(solve(i + 1, j + 1), 0) + nums1[i] * nums2[j]);
+        res = max(res, solve(i + 1, j));
+        res = max(res, solve(i, j + 1));
+        return dp[i][j] = res;
+    }
      int dp[501][501];
     vector<int> nums1;
     vector<int> nums2;
@@ -10,13 +19,5 @@ public:
         return solve(0, 0);
     }
 
-    int solve(int i, int j) {
-        if (i == nums1.size() || j == nums2.size()) return INT_MIN;
-        if (dp[i][j] != -1) return dp[i][j];
-        int res = INT_MIN;
-        res = max(res, max(solve(i + 1, j + 1), 0) + nums1[i] * nums2[j]);
-        res = max(res, solve(i + 1, j));
-        res = max(res, solve(i, j + 1));
-        return dp[i][j] = res;
-    }
+   
 };
